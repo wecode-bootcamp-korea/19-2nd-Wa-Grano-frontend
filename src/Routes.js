53b02c57e3067fe1b0a2f0ae4df8fe5276db3965
jinah2754 <jinah2754@gmail.com>
2021-04-26 15:50:41 +0900

    Add: 초기 셋팅 완료

---
+++ src/Routes.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Main from '../src/Pages/Main/Main';
+import SignUp from '../src/Pages/SignUp/SignUp';
+import Login from '../src/Pages/Login/Login';
+import ProductList from '../src/Pages/ProductList/ProductList';
+import ProductDetail from '../src/Pages/ProductDetail/ProductDetail';
+
+class Routes extends React.Component {
+  render() {
+    return (
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Main} />
+          <Route exact path="/sign-up" component={SignUp} />
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/product-list" component={ProductList} />
+          <Route exact path="/product-detail" component={ProductDetail} />
+        </Switch>
+      </Router>
+    );
+  }
+}
+export default Routes;
