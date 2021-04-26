ea5cdf1ecb60570fefe5074064d4cc1f7d627431
syeongiicoco <syeongii78@gmail.com>
2021-04-27 00:28:04 +0900

    리뷰반영

---
+++ src/Pages/Main/mainData.js
@@ -0,0 +1,11 @@
+const Busan = [
+  { id: 1, img: './MainImg/busanMain.jpg' },
+
+  { id: 2, img: './MainImg/busanvilage.jpg' },
+
+  { id: 3, img: './MainImg/kamchun.jpg' },
+
+  { id: 4, img: './MainImg/ocean.jpg' },
+];
+
+export default Busan;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Routes from './Routes';
+
 import './Styles/reset.scss';
 import './Styles/common.scss';
 
