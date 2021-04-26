e71833332764936e9fb67e3637606e0a67079b2b
jinah2754 <jinah2754@gmail.com>
2021-04-26 17:31:32 +0900

    index.js style 파일추가

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import Routes from "./Routes";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Routes from './Routes';
+import './Style/common.scss';
+import './Style/reset.scss';
 
-ReactDOM.render(<Routes />, document.getElementById("root"));
+ReactDOM.render(<Routes />, document.getElementById('root'));
