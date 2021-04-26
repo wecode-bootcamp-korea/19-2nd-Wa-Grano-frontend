1e97c0087c1360509b4f27943849d9a71ee773c9
syeongiicoco <syeongii78@gmail.com>
2021-04-27 00:28:04 +0900

    리뷰반영

--- src/Pages/Main/Main.js
+++ src/Pages/Main/Main.js
@@ -1,7 +1,41 @@
 import React, { Component } from 'react';
+import Busan from './mainData';
+import './Main.scss';
+
 class Main extends Component {
+  state = {
+    num: 0,
+    img: [],
+  };
+
+  componentDidMount() {
+    setInterval(() => {
+      const { num } = this.state;
+      this.setState({ num: num + 1 > 3 ? 0 : num + 1 });
+    }, 5000);
+  }
+
   render() {
-    return <div></div>;
+    const { num } = this.state;
+    return (
+      <div
+        className="slideBox"
+        style={{ backgroundImage: `url("${Busan[num].img}")` }}
+      >
+        <div className="slideSearch">
+          <p>부산의 명소를 검색해보세요.</p>
+          <div className="searchBox">
+            <i className="fas fa-search" />
+            <input
+              className="searchInput"
+              type="text"
+              placeholder="액티비티 또는 지역명으로 검색하세요"
+            />
+          </div>
+        </div>
+      </div>
+    );
   }
 }
+
 export default Main;
