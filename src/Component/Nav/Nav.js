764044f9ea5b6080e22e99f73fc9100dab71aaa9
jinah2754 <jinah2754@gmail.com>
2021-04-27 11:16:29 +0900

    수정사항 반영

--- src/Component/Nav/Nav.js
+++ src/Component/Nav/Nav.js
@@ -1,1 +1,19 @@
 import React, { Component } from 'react';
+import './Nav.scss';
+
+class Nav extends Component {
+  render() {
+    return (
+      <nav className="navBar">
+        <img className="logo" src="/images/logo.jpg" alt="로고이미지" />
+        <div className="navRightBar">
+          <button className="navRightLanguageBtn">한국어</button>
+          <button className="navRightWonBtn">KRW 대한민국 원(₩)</button>
+          <button className="navLogin">로그인</button>
+        </div>
+      </nav>
+    );
+  }
+}
+
+export default Nav;
